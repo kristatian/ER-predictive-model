9a4aa8e2d2b4ca3e5ab0f3d07711505b5f2180d4
Krista <kristatian@gmail.com>
2023-03-29 18:37:51 -0400

    Add prediction name column to table

--- backend/dbscript.sql
+++ backend/dbscript.sql
@@ -36,6 +36,7 @@ CREATE TABLE prediction(
     prediction VARCHAR(255),
     date_time TIMESTAMP,
     scenario_version_number INTEGER,
+    scenario_id VARCHAR(150) NOT NULL,
     PRIMARY KEY (request_id),
     FOREIGN KEY (scenario_id) REFERENCES what_if_scenario(scenario_id)
 );
--- backend/main.py
+++ backend/main.py
@@ -7,26 +7,33 @@ app.secret_key = "wetestingouthere"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-##Prediction Generation Function
+# Prediction Generation Function
+
+
 @app.route('/predict', methods=['POST'])
 def generate_prediction():
     if 'loggedin' in session:
         username = request.headers.get('username')
         scenario_id = request.headers.get('scenario_id')
         prediction_name = request.headers.get('prediction_name')
-        #model = request.headers.get('model')
+        # model = request.headers.get('model')
         request_body = request.get_json()
-        return ERPredict.generate_prediction(username, request_body,scenario_id, prediction_name) #return everything
+        # return everything
+        return ERPredict.generate_prediction(username, request_body, scenario_id, prediction_name)
     return "Not Logged in"
 
-#User
+# User
+
+
 @app.route('/user/register', methods=['GET', 'POST'])
 def create_user():
     request_body = request.get_json()
     print(request_body)
     return ERPredict.create_user(request_body)
 
-##Login
+# Login
+
+
 @app.route('/user/login', methods=['GET', 'POST'])
 def login():
     if 'loggedin' not in session:
@@ -35,7 +42,9 @@ def login():
         return ERPredict.verify_user(request_body)
     return "Already Logged In"
 
-##Logout
+# Logout
+
+
 @app.route('/user/logout')
 def logout():
     session.pop('loggedin', None)
@@ -43,15 +52,18 @@ def logout():
     session.pop('username', None)
     return "Success"
 
-#Prediction History
-@app.route('/history/get-history', methods=['GET']) ##Verified
+# Prediction History
+
+
+@app.route('/history/get-history', methods=['GET'])  # Verified
 def retrieve_history():
     if 'loggedin' in session:
         username = session['username']
         return ERPredict.retrieve_history(username)
     return "Not Logged in"
 
-@app.route('/history/remove-prediction', methods=['DELETE']) ##Verified
+
+@app.route('/history/remove-prediction', methods=['DELETE'])  # Verified
 def delete_prediction():
     if 'loggedin' in session:
         username = session['username']
@@ -59,15 +71,18 @@ def delete_prediction():
         return ERPredict.delete_prediction(username, prediction_id)
     return "Not Logged in"
 
-#What-If Scenarios
-@app.route('/scenario/get-scenarios', methods=['GET']) ##Verified
+# What-If Scenarios
+
+
+@app.route('/scenario/get-scenarios', methods=['GET'])  # Verified
 def retrieve_scenarios():
     if 'loggedin' in session:
         username = session['username']
         return ERPredict.retrieve_scenarios(username)
     return "Not Logged in"
 
-@app.route('/scenario/store-scenario', methods=['POST']) ##Verified
+
+@app.route('/scenario/store-scenario', methods=['POST'])  # Verified
 def store_scenarios():
     if 'loggedin' in session:
         username = session['username']
@@ -76,7 +91,8 @@ def store_scenarios():
         return ERPredict.store_scenarios(username, request_body, scenario_name)
     return "Not Logged in"
 
-@app.route('/scenario/remove-scenario', methods=['DELETE']) ##Verified
+
+@app.route('/scenario/remove-scenario', methods=['DELETE'])  # Verified
 def remove_scenario():
     if 'loggedin' in session:
         username = session['username']
@@ -84,7 +100,8 @@ def remove_scenario():
         return ERPredict.remove_scenario(username, scenario_id)
     return "Not Logged in"
 
-@app.route('/scenario/update-scenario', methods=['PATCH']) ##Verified
+
+@app.route('/scenario/update-scenario', methods=['PATCH'])  # Verified
 def update_scenario():
     if 'loggedin' in session:
         username = session['username']
@@ -93,6 +110,7 @@ def update_scenario():
         print(request_body)
         return ERPredict.update_scenario(username, scenario_id, request_body)
     return "Not Logged in"
+
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
